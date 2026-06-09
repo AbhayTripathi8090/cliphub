77a32584265a38510f4ad8837ef85b8a1c1bae5d
AbhayTripathi8090 <abhaytripathijuly15@gmail.com>
2026-06-10 00:00:16 +0530

    something did

--- models/Users.ts
+++ models/Users.ts
@@ -1,28 +1,28 @@
-    import mongoose, { Schema, model, models } from "mongoose";
-    import bcrypt from "bcryptjs";
+import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
 import next from "next/dist/server/next";
 
-    export interface IUser {
-    email: string;
-    password: string;
-    _id?: mongoose.Types.ObjectId;
-    createdAt?: Date;
-    updatedAt?: Date;
-    }
+export interface IUser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
-    const userSchema = new Schema<IUser>(
-    {
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-    },
-    { timestamps: true },
-    );
+const userSchema = new Schema<IUser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
-    userSchema.pre("save", async function () {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    });
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
 
-    const User = models?.User || model<IUser>("User", userSchema);
-    export default User;
+const User = models?.User || model<IUser>("User", userSchema);
+export default User;
